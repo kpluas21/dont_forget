a6ee5992dfd2d58aa4f7623c8d651049c0834ec5
Kevin Pluas <kpluas21@gmail.com>
2024-11-08 11:22:06 -0500

    feat: enhance local notification service with improved scheduling and initialization

--- lib/util/notifications.dart
+++ lib/util/notifications.dart
@@ -3,19 +3,32 @@ import 'package:timezone/timezone.dart' as tz;
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalNotificationService {
-  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
+  // Define the Android notification settings
+  final AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails('med_reminder', 'Medication Reminder',
+          channelDescription: 'Channel for medication reminders',
+          importance: Importance.max,
+          priority: Priority.high,
+          ticker: 'ticker');
+
   Future<void> init() async {
+    // Initialize the timezone
     tz.initializeTimeZones();
     
-    _flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+    // Request permissions for Android
+    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
     AndroidFlutterLocalNotificationsPlugin>()?.requestNotificationsPermission();
 
+
+    // Initialize the notification plugin
     final AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
 
-    await _flutterLocalNotificationsPlugin.initialize(
+    // Initialize the settings
+    await flutterLocalNotificationsPlugin.initialize(
       InitializationSettings(
         android: initializationSettingsAndroid,
       ),
@@ -23,41 +36,29 @@ class LocalNotificationService {
   }
 
   void showNotificationAndroid(String title, String value) async {
-    const AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails('channelId', 'channelName',
-            channelDescription: 'channelDescription',
-            importance: Importance.max,
-            priority: Priority.high,
-            ticker: 'ticker');
 
     int notificationId = 1;
-    const NotificationDetails notificationDetails =
+    NotificationDetails notificationDetails =
         NotificationDetails(android: androidNotificationDetails);
 
-    await _flutterLocalNotificationsPlugin.show(
+    await flutterLocalNotificationsPlugin.show(
         notificationId, title, value, notificationDetails,
         payload: 'item x');
   }
 
-  void scheduleNotificationAndroid(String title, String value) async {
-    const AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails('channelId', 'channelName',
-            channelDescription: 'channelDescription',
-            importance: Importance.max,
-            priority: Priority.high,
-            ticker: 'ticker');
 
-    int notificationId = 1;
-    const NotificationDetails notificationDetails =
+  // Schedule a notification for a specific time
+  void scheduleNotificationAndroid(int notificationId, String title, String value, DateTime scheduledTime) async {
+    NotificationDetails notificationDetails =
         NotificationDetails(android: androidNotificationDetails);
-
-    await _flutterLocalNotificationsPlugin.zonedSchedule(
-        notificationId,
-        title,
-        value,
-        tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
-        notificationDetails,
-        uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime, androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle);
+    
+    //Given a DateTime, convert it to a TZDateTime that is in the local timezone. 
+    final tz.TZDateTime tzScheduledTime = tz.TZDateTime.from(scheduledTime, tz.local);
+    
+    // Schedule the notification
+    await flutterLocalNotificationsPlugin.zonedSchedule(notificationId, title, value,
+        tzScheduledTime, notificationDetails,
+        androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+        uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime);
   }
 }
